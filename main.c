f1b936ddd2a7f333c36c1a6e1c616cea9519ab5b
Frank.Chiang <frank71726@gmail.com>
2015-01-06 10:36:44 +0800

    frank - communicate with ap og QT well

--- main.c
+++ main.c
@@ -43,19 +43,18 @@
 //none
 #endif
 
+INT16U gIcrDlyTime=0;
 INT8U gStart=0;
-INT8C MSG_init[]="hello world!";
-//INT8C MSG_left[]="a - LEFT";
-//INT8C MSG_leftest[]="The leftest";
-//INT8C MSG_rightest[]="The rightest";
-//INT8C MSG_right[]="d - RIGHT";
-//INT8C MSG_other[]="Please input 'a'(LEFT) or 'd'(RIGHT)";
+INT8C MSG_Start[]="$f00";//"$Please push start button#";
+INT8C MSG_init[]="$f01";//"$please input $00(all), $01(0 degree), $02(90 degree), $03(180 degree)#";
+INT8C MSG_AllTest[]="$f02";//"$Starting all test (0, 90, 180 degree)#";
+INT8C MSG_0_degree[]="$f03";//"$Starting 0 degree test#";
+INT8C MSG_90_degree[]="$f04";//"$Starting 90 degree test#";
+INT8C MSG_180_degree[]="$f05";//"$Starting 180 degree test#";
+INT8C MSG_Ok[]="$f06";//"$test OK#";
+INT8C MSG_Ng[]="$f07";//"$test NG#";
+INT8C MSG_Finish[]="$f08";
 
-//for Horizontal
-INT8U ASX340_offset_hi[4]={ 0x09, 0xC8, 0xFC, 0x00 };
-INT8U ASX340_offset_lo[4]={ 0x8E, 0x5C, 0x00, 0x40 };
-INT8U ASX340_offset_data[7]={ 0xC8, 0x5C, 0x00, 0x28, 0x00, 0x81, 0x00 };
-INT8U ASX340_offset_num[4]={ 0x02, 0x01, 0x02, 0x02 };
 //==========================================================================
 static void SensorInit( AutoMachine *am_val)
 //void SensorInit( AutoMachine *am_val)
@@ -87,135 +86,221 @@ static void McuInit( AutoMachine *am_val)
 	//falg setting
 	am_val->flag.mirror = 0;//no mirror
 }
-
 //==========================================================================
-void main(void)
+INT8U GetR232Value(AutoMachine *am)
 {
-	//AutoMachineClass automachineclass;
-	AutoMachine	 am;
-	INT8U	dir_udrl, rs232_temp, rec_count=0, rs232_rx_fifo_not_empty_flag ;
-	INT8U   temp=0, ir_count=0;
-	INT16S  icr_value=0;
- 
-	am.class = &__automachineclass; 
-	am.class->Cpu_Init(&am);
+	INT8U rs232_vlaue[3], temp, count=0, rs232_rx_fifo_not_empty_flag=0 ; 
+	INT8U return_value = 100;
 
-	while(1)
-	{
-		am.class->Cpu_UartWr(MSG_init);
-	}
+	while(1) {
+		rs232_rx_fifo_not_empty_flag = am->class->Cpu_GetUartFifoFlag(); 
 
-	am.class->Ir_Emit(_ON);
-	while(!gStart);
-	gStart = _OFF;
-	
-	while(1)
-	{
-		am.class->Icr_Ctrl(_ON);
-		am.class->Mcu_Dly(500);
-		icr_value = am.class->Icr_Read();
-		if(icr_value > icr_on_threshold)
-			ir_count++;
-		else
-		{
-			am.class->Led_Red(_OFF);//turn on red led => fail
-			while(!gStart);
-			ir_count = 0;
-			gStart = _OFF;
-			am.class->Led_Blue(_ON);//turn on blue led => success
-			am.class->Led_Red(_ON);//turn on red led => fail
-		}
-		
-		am.class->Icr_Ctrl(_OFF);
-		am.class->Mcu_Dly(500);
-		icr_value = am.class->Icr_Read();
-		if(icr_value < icr_off_threshold )
-			ir_count++;
-		else
-		{
-			am.class->Led_Red(_OFF);//turn on red led => fail
-			while(!gStart);
-			ir_count = 0;
-			gStart = _OFF;
-			am.class->Led_Blue(_ON);//turn on blue led => success
-			am.class->Led_Red(_ON);//turn on red led => fail
-
-		}
-
-		if(ir_count >= 10)
-		{
-			am.class->Led_Blue(_OFF);//turn on blue led => success
-			while(!gStart);
-			ir_count = 0;
-			gStart = _OFF;
-			am.class->Led_Blue(_ON);//turn on blue led => success
-			am.class->Led_Red(_ON);//turn on red led => fail
-		}
-	}
-}
-/*  	
-	am.class->Sen_Init(&am);
-	
-	am.class->Cpu_UartWr(MSG_init);
-	while(1)
-	{
-		rs232_rx_fifo_not_empty_flag = am.class->Cpu_GetUartFifoFlag(); 
-		if( rs232_rx_fifo_not_empty_flag == 1 )
-		{
-			rs232_temp = am.class->Cpu_GetUartFifoData();
-			
-			switch ( rec_count )
-			{
-				case 0 :
-					if((rs232_temp == 'H')||(rs232_temp == 'V'))
-					{
-						dir_udrl = rs232_temp;
-						rec_count++; 
+		if( rs232_rx_fifo_not_empty_flag == 1 ) {
+			switch(count) {
+				case 0:
+					temp = am->class->Cpu_GetUartFifoData();
+					if('$' == temp) {
+						rs232_vlaue[count] = temp;
+						count++;
 					}
 					break;
-				case 1 :
-					switch ( dir_udrl )
-					{
-						case 'V' :
-							if(((rs232_temp>= 0x00)&&(rs232_temp<= 0x24)) ||\ 
-								((rs232_temp<= 0xFF)&&(rs232_temp>= 0xDC)))
-							{ //sending i2c to sensor optical_offset_Vertical
-								ASX340_offset_data[1] = 0x5d;//for verticalo
-								ASX340_offset_lo[1]=0x5d;
-								ASX340_offset_data[2] = rs232_temp;
-							}
-							else
-								rec_count = 0;
-							break;
-						case 'H' :
-							if(((rs232_temp>= 0x00)&&(rs232_temp<= 0x29)) ||\ 
-								((rs232_temp<= 0xFF)&&(rs232_temp>= 0xD7)))
-							{ //sending i2c to sensor optical_offset_Herizontal
-								ASX340_offset_data[1] = 0x5c;//for herizontal 
-								ASX340_offset_lo[1]=0x5c;
-								ASX340_offset_data[2] = rs232_temp;
-							}
-							else
-								rec_count = 0;
-							break;
-						default :
-							rec_count = 0;
-							break;
-
-					}
-					if( rec_count != 0 )
-						am.class->Sen_RamWrite( ASX340_offset_hi, ASX340_offset_lo, ASX340_offset_data,\
-							ASX340_offset_num, sizeof(ASX340_offset_hi)/sizeof(ASX340_offset_hi[0]) );
-
-					rec_count = 0;
+				case 1:
+					rs232_vlaue[count] = am->class->Cpu_GetUartFifoData();
+					count++;
 					break;
-				default :
+				case 2:
+					rs232_vlaue[count] = am->class->Cpu_GetUartFifoData();
+					count++;
+					switch(rs232_vlaue[1]) {
+						case '0':
+							switch(rs232_vlaue[2]) {
+								case '0':
+									return_value = 0;//all test
+									break;
+								case '1':
+									return_value = 1;//moving 0 degree 
+									break;
+								case '2':
+									return_value = 2; //moving 90 degree
+									break;
+								case '3':
+									return_value = 3; //moving 180 degree
+									break;
+								default:
+									return_value = 100;//NG
+									break;
+							}
+							break;
+						case '9':
+							switch(rs232_vlaue[2]) {
+								case '0':
+									return_value = 200;//button start
+									break;
+								case '1':
+									return_value = 201;//button reset 
+									break;
+								default:
+									break;
+							}
+							break;
+						case 'A':
+							switch(rs232_vlaue[2]) {
+								case '0': gIcrDlyTime = 100; break;
+								case '1': gIcrDlyTime = 200; break;
+								case '2': gIcrDlyTime = 300; break;
+								case '3': gIcrDlyTime = 400; break;
+								case '4': gIcrDlyTime = 500; break;
+								case '5': gIcrDlyTime = 600; break;
+								case '6': gIcrDlyTime = 700; break;
+								case '7': gIcrDlyTime = 800; break;
+								case '8': gIcrDlyTime = 900; break;
+								case '9': gIcrDlyTime = 1000; break;
+								default:			      break;
+							}
+							break;
+						default:
+							return_value = 100;//NG 
+							break;
+					}
+					return return_value;
+					break;
+				default:
+					return_value = 100;//NG 
+					return return_value;
 					break;
 			}
 		}
 	}
 }
-*/	
+//==========================================================================
+INT8U wait_start(AutoMachine *am)
+{
+	INT8U temp;
+	while(1)
+	{
+		temp = GetR232Value(am);
+		  	
+		if(temp == 200 )
+			return 0;
+		else if(temp == 201 )
+		{
+			Reset();
+		}
+		else
+			return 1;
+	}
+}
+//==========================================================================
+void StartIcrTest(AutoMachine *am)
+{
+	INT8U   ir_count=0;
+	INT16S  icr_value=0;
+
+	while(1)
+	{
+		am->class->Icr_Ctrl(_ON);
+		am->class->Mcu_Dly(gIcrDlyTime);
+		icr_value = am->class->Icr_Read();
+		if(icr_value > icr_on_threshold)
+			ir_count++;
+		else
+		{
+			am->class->Led_Red(_OFF);//turn on red led => fail
+			am->class->Cpu_UartWr(MSG_Ng);//"$ng"
+			while(wait_start(am));
+			ir_count = 0;
+			am->class->Led_Blue(_ON);//turn on blue led => success
+			am->class->Led_Red(_ON);//turn on red led => fail
+		}
+		
+		am->class->Icr_Ctrl(_OFF);
+		am->class->Mcu_Dly(gIcrDlyTime);
+		icr_value = am->class->Icr_Read();
+		if(icr_value < icr_off_threshold )
+			ir_count++;
+		else
+		{
+			am->class->Led_Red(_OFF);//turn on red led => fail
+			am->class->Cpu_UartWr(MSG_Ng);//"$ng"
+			while(wait_start(am));
+			ir_count = 0;
+			am->class->Led_Blue(_ON);//turn on blue led => success
+			am->class->Led_Red(_ON);//turn on red led => fail
+		}
+
+		if(ir_count >= 6)
+		{
+			am->class->Led_Blue(_OFF);//turn on blue led => success
+			am->class->Cpu_UartWr(MSG_Ok);
+			am->class->Mcu_Dly(1000);
+			ir_count = 0;
+			am->class->Led_Blue(_ON);//turn on blue led => success
+			am->class->Led_Red(_ON);//turn on red led => fail
+			break;
+		}
+	}
+}
+//==========================================================================
+void main(void)
+{
+	//AutoMachineClass automachineclass;
+	AutoMachine	 am;
+	INT8U	temp=0;
+ 
+	am.class = &__automachineclass; 
+	am.class->Cpu_Init(&am);
+
+	am.class->Cpu_UartWr(MSG_Start);
+	am.class->Ir_Emit(_ON);
+
+	while(wait_start(&am));
+	am.class->Cpu_UartWr(MSG_init);
+
+	while(1)
+	{
+		switch(GetR232Value(&am))
+		{
+			case 0://start
+				am.class->Cpu_UartWr(MSG_AllTest);
+				//rotate 0 degree
+				am.class->Cpu_UartWr(MSG_0_degree);
+				StartIcrTest(&am);
+				//rotate 90 degree
+				am.class->Cpu_UartWr(MSG_90_degree);
+				StartIcrTest(&am);
+				//rotate 180 degree
+				am.class->Cpu_UartWr(MSG_180_degree);
+				StartIcrTest(&am);
+				am.class->Cpu_UartWr(MSG_Finish);
+				break;
+			case 1://moving 0 degree
+				//rotate 0 degree
+				am.class->Cpu_UartWr(MSG_0_degree);
+				StartIcrTest(&am);
+				am.class->Cpu_UartWr(MSG_Finish);
+				break;
+			case 2://moving 90 degree
+				//rotate 90 degree
+				am.class->Cpu_UartWr(MSG_90_degree);
+				StartIcrTest(&am);
+				am.class->Cpu_UartWr(MSG_Finish);
+				break;
+			case 3://moving 180 degree
+				//rotate 180 degree
+				am.class->Cpu_UartWr(MSG_180_degree);
+				StartIcrTest(&am);
+				am.class->Cpu_UartWr(MSG_Finish);
+				break;
+			case 201:
+				Reset();
+				break;
+			default:
+				//am.class->Cpu_UartWr(MSG_Ng);//"$ng"
+				break;
+		}
+		am.class->Cpu_UartWr(MSG_init);
+	}
+}
 /* 
 //==========================================================================
 void puthexUart(INT8U HEX_Val, AutoMachine *am_val )
@@ -237,37 +322,4 @@ void puthexUart(INT8U HEX_Val, AutoMachine *am_val )
 
 	am_val->class->Cpu_UartWr(Temp_HEX);
 }
-//==========================================================================
-
-if((rs232_data  == 'a') || (rs232_data  == 'd'))
-{
-	if(rs232_data  == 'a')
-	{
-		am.class->Cpu_UartWr(MSG_left);
-		if( offset == 0x00 )
-			offset = 0xFF;
-		else if( offset == 0xD7 )
-			am.class->Cpu_UartWr(MSG_leftest);
-		else
-			offset--;
-	}
-	else if(rs232_data  == 'd')
-	{
-		am.class->Cpu_UartWr(MSG_right);
-		if( offset == 0xFF )
-			offset = 0x00;
-		else if( offset == 0x29 )
-			am.class->Cpu_UartWr(MSG_rightest);
-		else
-			offset++;
-	}
-	//sending step to uart
-	puthexUart(offset, &am);
-	//sending i2c to sensor
-	ASX340_offset_data[2] = offset;
-	am.class->Sen_RamWrite( ASX340_offset_hi, ASX340_offset_lo, ASX340_offset_data,\
-			ASX340_offset_num, sizeof(ASX340_offset_hi)/sizeof(ASX340_offset_hi[0]) );
-}
-else
-	am.class->Cpu_UartWr(MSG_other);
 */
